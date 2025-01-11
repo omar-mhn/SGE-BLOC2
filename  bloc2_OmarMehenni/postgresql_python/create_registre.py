efd650500bdd452ed3f030d23a2564c74d99eeea
omar mehenni <2024_omar.mehenni@iticbcn.cat>
2025-01-11 14:19:56 +0100

    hecho todo el codigo hasta el punto 8

---
+++  bloc2_OmarMehenni/postgresql_python/create_registre.py
@@ -0,0 +1,25 @@
+import connect
+
+# fonction pour créer un enregistrement dans la DB avec une requête préparée
+def create_reg():
+    # créer la connexion et la garde dans une variable conn
+    conn = connect.connection_db()
+
+    #créer un curseur avec la conneion gardée dans la variable conn
+    cursor = conn.cursor()
+
+   #Consulta preparada amb %s
+    sql_create = "INSERT INTO Clientes (nombre_cliente, dirección_cliente, teléfono_cliente, correo_electrónico_cliente, fecha_cumpleaños) VALUES (%s, %s, %s, %s, %s);"
+
+    # Valeurs à ajouter, dans l'ordre, aux %s de VALUES de la requête préparée.
+    values=('Roger', 'carrer el que sigui', '678113452', 'correu@correu.com', '12_09_1999')
+    
+    # maintenant envoyer la consultation preparé avec les valeurs utulisant le curseur 
+    cursor.execute(sql_create,values)
+
+    # faire les modifications a la BD selon execute()
+    conn.commit()
+
+    # fermer les connexions 
+    conn.close()
+    cursor.close()
